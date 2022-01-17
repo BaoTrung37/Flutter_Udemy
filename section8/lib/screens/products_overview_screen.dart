7c1332f708707a020264b51586eea558b4fc9de1
baotrung37 <you@example.combaotrung372k@gmail.com>
2022-01-17 22:26:46 +0700

    211: Using a Slide Drawer

--- section8/lib/screens/products_overview_screen.dart
+++ section8/lib/screens/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../widgets/app_drawer.dart';
 import 'cart_screen.dart';
 import '../providers/cart.dart';
 import '../widgets/product_grid.dart';
@@ -66,6 +67,7 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(
         showFavorites: showOnlyFavorites,
       ),
