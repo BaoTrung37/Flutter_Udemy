cdcd03b7178ce857e7c510f93eaf50efb076a202
baotrung37 <you@example.combaotrung372k@gmail.com>
2022-01-18 22:29:52 +0700

    Using Forms & Working with Forms Input

--- section8/lib/screens/user_products_screen.dart
+++ section8/lib/screens/user_products_screen.dart
@@ -3,6 +3,8 @@ import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+import './edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
@@ -20,11 +22,13 @@ class UserProductsScreen extends StatelessWidget {
           IconButton(
             onPressed: () {
               //TODO: implement logout
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
             },
             icon: const Icon(Icons.add),
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
