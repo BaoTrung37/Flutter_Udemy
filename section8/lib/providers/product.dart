b0e4276bb90293104be3824e0b447f82269f12b2
baotrung37 <you@example.combaotrung372k@gmail.com>
2022-01-21 15:59:09 +0700

    255: Update the favorites status optimisically

--- section8/lib/providers/product.dart
+++ section8/lib/providers/product.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -7,6 +9,7 @@ class Product with ChangeNotifier {
   final double price;
   final String imageUrl;
   bool isFavorite;
+
   Product({
     required this.id,
     required this.title,
@@ -15,8 +18,31 @@ class Product with ChangeNotifier {
     required this.imageUrl,
     this.isFavorite = false,
   });
-  void toggleFavoritesStatus() {
+
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoritesStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    try {
+      final url = Uri.parse(
+          "https://flutter-app-82f7b-default-rtdb.firebaseio.com/products/$id.json");
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+      // notifyListeners();
+    } catch (e) {
+      _setFavValue(oldStatus);
+    }
   }
 }
