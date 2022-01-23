2093f8ee2dbfc5c48eac2781175f7fa84986001a
baotrung37 <you@example.combaotrung372k@gmail.com>
2022-01-23 16:46:20 +0700

    end section 9

--- section8/lib/screens/orders_screen.dart
+++ section8/lib/screens/orders_screen.dart
@@ -5,50 +5,44 @@ import '../widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    // * Có thể sử dùng Provider.of<Orders>(context) bởi vì mình chỉ dùng dễ hiện thị chứ ko lắng nghe
-
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    print('building orders');
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders!'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : Container(
-              child: ListView.builder(
-              itemBuilder: (ctx, index) =>
-                  OrderItem(order: orderData.orders[index]),
-              itemCount: orderData.orders.length,
-            )),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              // ?
+              return const Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemBuilder: (ctx, index) =>
+                      OrderItem(order: orderData.orders[index]),
+                  itemCount: orderData.orders.length,
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
--- section8/lib/screens/user_products_screen.dart
+++ section8/lib/screens/user_products_screen.dart
@@ -19,6 +19,7 @@ class UserProductsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // ! lỗi chỗ này
     final String id = "_";
+    print('building orders');
     final productData = Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
