f59550f2680950060551d9203d5318427a57c0b8
baotrung37 <you@example.combaotrung372k@gmail.com>
2022-01-24 21:55:42 +0700

    268: Handing authentication errors

--- section8/lib/providers/auth.dart
+++ section8/lib/providers/auth.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   late String _token;
@@ -10,19 +11,29 @@ class Auth with ChangeNotifier {
 
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
-    final url = Uri.parse(
-        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyAAiXoR0mAMWLaWsgD6RzwtQH_szGGyt08");
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    try {
+      final url = Uri.parse(
+          "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyAAiXoR0mAMWLaWsgD6RzwtQH_szGGyt08");
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      print(json.decode(response.body));
+
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(message: responseData['error']['message']);
+      }
+    } catch (e) {
+      rethrow;
+      // throw e;
+    }
   }
 
   Future<void> singup(String email, String password) async {
