47313e2fa416a96103c4846f91b8aff292cbe2f3
baotrung37 <you@example.combaotrung372k@gmail.com>
2022-02-11 16:34:06 +0700

    295: setup

---
+++ section13/lib/models/place.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+class PlaceLoction {
+  final double latitude;
+  final double longtitude;
+  final String address;
+
+  PlaceLoction({
+    required this.latitude,
+    required this.longtitude,
+    required this.address,
+  });
+}
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLoction location;
+  final File image;
+  Place({
+    required this.id,
+    required this.title,
+    required this.location,
+    required this.image,
+  });
+}
