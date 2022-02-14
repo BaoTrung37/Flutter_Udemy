c30cb3c93055b77534d28c1f9aed19783cb71019
baotrung37 <you@example.combaotrung372k@gmail.com>
2022-02-14 16:09:19 +0700

    316: add firebase

--- section14/lib/screens/chat_screen.dart
+++ section14/lib/screens/chat_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -12,6 +14,17 @@ class ChatScreen extends StatelessWidget {
           child: Text('This Works'),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            // Firebase.initializeApp();
+            FirebaseFirestore.instance
+                .collection('/chats/GdNtT97DBf7SsB0WNyW8/messages')
+                .snapshots()
+                .listen((data) {
+              data.docs.map((doc) => print(doc['text'])).toString();
+            });
+          }),
     );
   }
 }
